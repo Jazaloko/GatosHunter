3937090fa40980ba144d259c01c54f1ddec291c9
Jazaloko <Jazaelvaleron76@gmail.com>
2025-05-12 20:00:10 +0100

    Pequeños cambios

--- app/src/main/java/com/example/gatoshunter/Login.kt
+++ app/src/main/java/com/example/gatoshunter/Login.kt
@@ -3,6 +3,7 @@ package com.example.gatoshunter
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class Login : AppCompatActivity() {
@@ -11,8 +12,10 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val loginButton: Button = findViewById("x")
-        val registerButton: Button = findViewById("x")
+        val loginButton: Button = findViewById(R.id.buttonLogin)
+        val registerButton: Button = findViewById(R.id.buttonRegister)
+        val userName: EditText = findViewById(R.id.userName)
+        val password: EditText = findViewById(R.id.password)
 
         //Funcion para redirigir a la pagina de Registrar
         registerButton.setOnClickListener {
@@ -22,6 +25,9 @@ class Login : AppCompatActivity() {
         }
 
         loginButton.setOnClickListener {
+            if (userName.text.isNullOrEmpty() || userName.text.isNullOrBlank() && password.text.isNullOrBlank() || userName.text.isNullOrEmpty()){
+                //Mostrar un mensaje de error por que faltan datos
+            }
             //Mandar a verificar los datos del login, en caso correcto, redirigir al MainActivity, en caso contrario, Salte mensaje de error
         }
     }
